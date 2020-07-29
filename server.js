5f747911eaa643e073d01fccd47b1116a824eff4
kumol <kumol0163@gmail.com>
2020-07-30 03:52:27 +0600

    Socket implemented

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ userRoute = require('./api/routes/user/user');
 var errHandler = require('./config/err');
 var express = require('express');
 var app = express();
+const server = require('http').createServer(app);
+const socketio = require("socket.io");
+const io = socketio(server);
+
+server.listen(process.env.PORT,()=>{
+    console.log(`Server starts at port ${process.env.PORT}`);
+})
 var cors = require('cors');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
-app.listen(process.env.PORT,()=>{
-    console.log("server starts at port ",process.env.PORT);
-});
+
+io.on('connection',(socket)=>{
+    console.log("We have a new connection");
+    socket.on("disconnect",()=>{
+        console.log("User had left!!!");
+    })
+})
+// app.listen(process.env.PORT,()=>{
+//     console.log("server starts at port ",process.env.PORT);
+// });
 app.use('/admin',adminRoute);
 app.use('/user',userRoute);
 app.use(errHandler.one);
