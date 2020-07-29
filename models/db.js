413c6b8d5be64f4a1c1279d73c2f7eef7db3e199
kumol <kumol0163@gmail.com>
2020-07-30 03:08:11 +0600

    database connected

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var config = require('../config/config')
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI,(err)=>{
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true },(err)=>{
     if(!err){
         console.log('Database connect successfully');
     }else{
