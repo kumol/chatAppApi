f990998e5add2a4ada0ff025ced617c9624c78b5
kumol <kumol0163@gmail.com>
2020-07-30 06:01:09 +0600

    controller added to hold all logics

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('./models/db');
 adminRoute = require('./api/routes/admin/admin');
 userRoute = require('./api/routes/user/user');
 var errHandler = require('./config/err');
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./controllers/user/users');
 var express = require('express');
 var app = express();
 const server = require('http').createServer(app);
@@ -20,9 +21,17 @@ app.use(cors());
 
 io.on('connection',(socket)=>{
     console.log("We have a new connection");
+    socket.on('join',({name,room},callback)=>{
+        const {error,user}=addUser({id:socket.id,name,room});
+        if(error) return callback(error);
+        socket.emit("message",{user:"admin",text:`${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit("message",{user:"admin",text: `${user.name}, has joined`})
+        socket.join(user.room);
+        callback();
+    });
     socket.on("disconnect",()=>{
         console.log("User had left!!!");
-    })
+    });
 })
 // app.listen(process.env.PORT,()=>{
 //     console.log("server starts at port ",process.env.PORT);
